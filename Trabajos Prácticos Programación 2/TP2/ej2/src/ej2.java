e42e0038d9c222e144ad44c0f75a6c9eea9e738a
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-08-15 23:55:44 -0300

    Subo ej2

---
+++ Trabajos Prácticos Programación 2/TP2/ej2/src/ej2.java
@@ -0,0 +1,14 @@
+/*
+Determinar el Mayor de Tres Números. 
+Escribe un programa en Java que pida al usuario tres números enteros
+y determine cuál es el mayor. 
+Ejemplo de entrada/salida: 
+Ingrese el primer número: 8 
+Ingrese el segundo número: 12 
+Ingrese el tercer número: 5 
+El mayor es: 12
+ */
+
+public class ej2 {
+    
+}
