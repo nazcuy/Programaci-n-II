e6cfa2d09443ff6ab94e60d894d388c8e9927966
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-08-16 00:26:01 -0300

    Subo ejercicio 2

--- Trabajos Prácticos Programación 2/TP2/ej2/src/ej2.java
+++ Trabajos Prácticos Programación 2/TP2/ej2/src/ej2.java
@@ -1,14 +1,28 @@
-/*
-Determinar el Mayor de Tres Números. 
-Escribe un programa en Java que pida al usuario tres números enteros
-y determine cuál es el mayor. 
-Ejemplo de entrada/salida: 
-Ingrese el primer número: 8 
-Ingrese el segundo número: 12 
-Ingrese el tercer número: 5 
-El mayor es: 12
- */
-
+import java.util.Scanner;
 public class ej2 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int[] numeros = new int[3];
+        
+        for (int i = 0; i < 3; i++) {
+            String orden;
+            if (i == 0) {
+                orden = "primer";
+            } else if (i == 1) {
+                orden = "segundo";
+            } else {
+                orden = "tercero";
+            }
+            System.out.print("Ingrese el " + orden + " número: ");
+            numeros[i] = entrada.nextInt();        
+        }    
+    int mayor = numeros[0];
+    if (numeros[1] > mayor) {
+        mayor = numeros[1];
+    }
+    if (numeros[2] > mayor) {
+        mayor = numeros[2];
+    }
+    System.out.println("El número mayor es: " + mayor);
+    }
 }
