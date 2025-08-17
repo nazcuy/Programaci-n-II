a6317883d647ae813445e59979fdac6e4c6f6f20
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-08-17 19:09:54 -0300

    Subo ejercico 11

--- Trabajos Prácticos Programación 2/TP2/ej11/src/ej11.java
+++ Trabajos Prácticos Programación 2/TP2/ej11/src/ej11.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author nicoa
- */
+import java.util.Scanner;
 public class ej11 {
-    
+    static double porcentajeDescuento = 0.10;
+    public static void calcularDescuentoEspecial(double precio) {
+        double descuentoAplicado = precio * porcentajeDescuento;
+        double precioFinal = precio - descuentoAplicado;
+        System.out.println("El descuento especial aplicado es: " + descuentoAplicado);
+        System.out.println("El precio final con descuento es: " + precioFinal);
+    }
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el precio del producto: ");
+        double precio = entrada.nextDouble();
+        calcularDescuentoEspecial(precio);
+    }
 }
