0650dae116c5c49d2856443ffb8a8efb32c3508b
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-08-17 20:05:10 -0300

    Subo ejercico 13. Listo para entregar.

--- Trabajos Prácticos Programación 2/TP2/ej13/src/ej13.java
+++ Trabajos Prácticos Programación 2/TP2/ej13/src/ej13.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 public class ej13 {
+    public static void mostrarPrecios(double[] precios, int indice) {
+        if (indice < precios.length) {
+            System.out.println("Precio: " + precios[indice]);
+            mostrarPrecios(precios, indice + 1);
+        }
+    }
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         double[] precios = {199.99, 299.5, 149.75, 399.0, 89.99};   
         System.out.println("Precios originales: ");
-        for (double precio : precios) {
-            System.out.println("Precio: $" + precio);
-        }
+        mostrarPrecios(precios, 0);
+        
         System.out.print("Ingrese el precio a modificar: ");
         double precioViejo = entrada.nextDouble();
         System.out.print("Ingrese el nuevo precio: ");
@@ -24,7 +29,6 @@ public class ej13 {
             System.out.println("El precio ingresado no se encontró entre los precios.");
         }
         System.out.println("Precios Modificados: ");
-        for (double precio : precios) {
-            System.out.println("Precio: $" + precio);
+        mostrarPrecios(precios, 0);
         }
-    }
+    } 
