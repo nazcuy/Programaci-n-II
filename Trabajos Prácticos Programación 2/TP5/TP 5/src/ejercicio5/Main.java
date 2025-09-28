102ccc2a8f0d4f096ad3a0e494071026afa862a3
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-09-28 20:40:56 -0300

    Subo ejercicio 5

---
+++ Trabajos Prácticos Programación 2/TP5/TP 5/src/ejercicio5/Main.java
@@ -0,0 +1,17 @@
+package ejercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+        Computadora computadora = new Computadora("HP", "123456789", "P1945", "AMD");
+        Propietario propietario = new Propietario("Azcuy", "12345678");
+        
+        computadora.setPropietario(propietario);
+        
+        System.out.println("=== INFORMACIÓN DE LA COMPUTADORA ===");
+        System.out.println(computadora.getMarca() + " " + computadora.getNumeroSerie());
+        System.out.println(computadora.getPlacaMadre().getModelo() + " " + computadora.getPlacaMadre().getChipset());
+ 
+        System.out.println("\n=== INFORMACIÓN DEL PROPIETARIO ===");
+        System.out.println(propietario.getNombre() + " " + propietario.getDni());
+    }
+}
