25534bc83119737209995a5e6a48cd258dec8461
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-09-28 22:56:07 -0300

    Subo ejercicio 6

---
+++ Trabajos Prácticos Programación 2/TP5/TP 5/src/ejercicio6/Main.java
@@ -0,0 +1,16 @@
+package ejercicio6;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Azcuy", "1123456789");
+        Mesa mesa = new Mesa(1,4);
+        Reserva reserva = new Reserva("28/09/2025", "22:50");
+        
+        reserva.setCliente(cliente);
+        reserva.setMesa(mesa);
+        
+        System.out.println("=== INFORMACIÓN DE LA RESERVA ===");
+        System.out.println("Cliente: " + reserva.getCliente().getNombre());
+        System.out.println("Mesa: " + reserva.getMesa().getNumero());
+    }
+}
