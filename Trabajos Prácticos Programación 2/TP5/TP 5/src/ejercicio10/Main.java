0a9a1b4aedf6ef69d6475ada073677b664ec2594
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-10-02 20:05:56 -0300

    Subo ejercicio 10

--- Trabajos Prácticos Programación 2/TP5/TP 5/src/ejercicio10/Main.java
+++ Trabajos Prácticos Programación 2/TP5/TP 5/src/ejercicio10/Main.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicio10;
 
-/**
- *
- * @author nicoa
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria("123456", 50000.0, "ABC123", "02/10/2025");
+        Titular titular = new Titular("Nicolás Azcuy", "12345678");
+
+        cuenta.setTitular(titular);
+        System.out.println("Titular de la cuenta: " + cuenta.getTitular().getNombre());
+        System.out.println("Código de seguridad: " + cuenta.getClave().getCodigo());
+    }
 }
